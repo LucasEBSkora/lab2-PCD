5903f92c7d8c0640d8ad24263d57c7a084ab866a
LucasEBSkora <skora.lucas@gmail.com>
2023-12-15 10:54:32 -0300

    ajout e mise en forme projet couche

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module eu.telecomnancy.lab2e2455u {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens eu.telecomnancy.lab2e2455u to javafx.fxml;
+    exports eu.telecomnancy.lab2e2455u;
+}
